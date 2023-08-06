38b8b465037fe6e31bd1fda2224528a05c5198c6
Yeusint <Yeusint@DESKTOP-H55R6AP>
2023-08-06 23:41:03 +0800

    添加项目文件。

---
+++ Study/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Study
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Check1.IsChecked == true)
+            {
+                MessageBox.Show("你选了" + Check1.Content);
+            }else if(Check2.IsChecked == true)
+            {
+                MessageBox.Show("你选了" + Check2.Content);
+            }
+            else { MessageBox.Show("你干嘛不选?"); }
+            if (String.IsNullOrWhiteSpace(Input.Text))
+            {
+                MessageBox.Show("输入框未输入");
+            }
+            else { MessageBox.Show("输入框的内容为" + Input.Text); }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import bin.t;
+import bin.et;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(f(t.t_3(3.9, 6f, 5), 7f));
+        new t(7.9f, 9);
+        new et(7.9f, 9);
+        System.out.println(t.t_1(95));
+        System.out.println(t.t_3(6.6, 2.4f, 8)*et.t_1(6));
+    }
+
+    public static float f(double a, float b){
+        return (float) (a+b);
+    }
+}
